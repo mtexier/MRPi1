fb5d6bf03de0728e659fb3bf5830a37ed59118c1
macerobotics <contact@macerobotics.com>
2016-03-14 14:44:37 +0100

    Ajouté lib python robot mrpi1

---
+++ Software/mrpi1_lib/README.md
@@ -0,0 +1,15 @@
+MRPi1 Lib - MRPi1 python library
+========================================================
+
+install mrpi1_lib
+
+>> sudo python setup.py install
+
+
+
+Use exemple :
+
+    >>> from mrpi1_lib import *
+    >>> forward(20)
+
+MIT Licence
---
+++ Software/mrpi1_lib/mrpi1_lib/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+    Ce module proclame la bonne parole de sieurs Sam et Max. Puissent-t-ils
+    retrouver une totale liberté de pensée cosmique vers un nouvel age
+    reminiscent.
+"""
+ 
+__version__ = "0.0.1"
+ 
+from mrpi1 import led
+from mrpi1 import ledToggle
+
+from mrpi1 import firmwareVersion
+from mrpi1 import switch
+
+from mrpi1 import battery
+from mrpi1 import temperature
+from mrpi1 import groundSensor
+from mrpi1 import ambiantLight
+from mrpi1 import proxSensor
+
+from mrpi1 import forward
+from mrpi1 import forwardC
+from mrpi1 import back
+from mrpi1 import backC
+from mrpi1 import stop
+from mrpi1 import turnLeft
+from mrpi1 import turnLeftC
+from mrpi1 import turnRight
+from mrpi1 import turnRightC
+
+from mrpi1 import motorRight
+from mrpi1 import motorLeft
+
+from mrpi1 import encoderLeft
+from mrpi1 import encoderRight
+
+from mrpi1 import playWav
+from mrpi1 import playTxt
+from mrpi1 import play
+
+from mrpi1 import writeCommand
+from mrpi1 import readData
+
+
